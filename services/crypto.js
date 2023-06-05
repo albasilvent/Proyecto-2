847828cd6cbb15f9b01e82e202457d476e0de943
albasilvent <albasilvent@gmail.com>
2023-06-05 16:39:15 +0200

    Funciones crypto

--- services/crypto.js
+++ services/crypto.js
@@ -1,13 +1,40 @@
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+const jwt= require("jsonwebtoken");
+
+
 //Funcion para hashear la contraseña
 //Se usa en los insert de la base de datos y en el registro ????
+
+async function hashPassword(password){
+    let hashedPassword = await bcrypt.hash(password, 12)
+    return hashedPassword;
+}
+
 
 //Funcion para validar la contraseña
 //Se usa en el login
 
+async function validatePassword(password, hash){
+    let compare = await bcrypt.compare(password, hash);
+    return compare;
+}
+
+
 //Generar un numero para validar emails
 //se usa en el registro
+
+function generateValidationCode() {
+    let validationCode = crypto.randomInt(100000, 999999);
+    return validationCode;
+}
+
 
 //Generar UUID
 //Se usa en los insert de la base de datos, en añadir comentarios y en añadir posts
 //Tambien se usa para el like
 
+function generateUUID() {
+    let uuid = crypto.randomUUID();
+    return uuid;
+}
