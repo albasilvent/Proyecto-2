b8271ed2dc8bc781bb069f9e56ddee09ce164859
HACKER <nescaruncho@gmail.com>
2023-06-06 17:44:26 +0200

    base de datos

--- database/init.js
+++ database/init.js
@@ -1,7 +1,16 @@
-//Iniciar la base de datos (conseguir la conexion)
+//Conectar la base de datos
+//Requerimos la funcion de init.js y la llamamos getDb.
 
-//Requerir mysql2
+require("dotenv").config();
+const cryptoService = require("../services/crypto.js");
+const { createPool } = require("./connection.js");
+const { faker } = require("@faker-js/faker");
 
-//Creamos la variable pool
-//Creamos una funcion "getDB". Si la pool no existe, la crea
-//Si existe: return await pool.getConnection();
+//Creamos una funcion. Dentro de la funcion creamos una variable
+//En esa variable almacenamos la llamada a la funcion
+
+//Esa variable sirve para conectarnos con la db. Con ella creamos las querys
+//Meter la funcion de crear tablas.
+//Metemos la funcion de crearInserts
+//Metemos la funcion de crearData
+//Cerramos la conexion ???????
