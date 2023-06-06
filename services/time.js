840d8ec9880acfcd5395d0d20d8ecac8610abd52
albasilvent <albasilvent@gmail.com>
2023-06-06 17:20:16 +0200

    Cambios en images.js, time.js

--- services/time.js
+++ services/time.js
@@ -1,6 +1,7 @@
 //Funcion que devuelve un timestamp x cantidad de minutos en el futuro
-module.exports = {
-    getTimestampMinutesFromNow(minutes) {
-      return Date.now() + minutes * 60 * 1000;
-    },
-  };
+    function getTimestampMinutesFromNow(minutes) {
+      let timestamp= Date.now() + minutes * 60 * 1000
+      return timestamp;
+    }
+
+    module.exports= {getTimestampMinutesFromNow};
